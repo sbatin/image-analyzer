b278044bab91e38d77d7523352f51289e4eec5ef
Sergey Batin <the.batin@gmail.com>
2023-09-25 22:25:56 -0700

    tune settings dialog

--- client/src/Settings.vue
+++ client/src/Settings.vue
@@ -38,12 +38,12 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Ananlyze Settings</h5>
+          <h5 class="modal-title">Image processing options</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <div class="mb-3">
-            <label for="hashType" class="form-label">Hash type</label>
+            <label for="hashType" class="form-label">Algorithm</label>
             <select id="hashType" class="form-select" v-model="hashType">
               <option value="AHash">aHash</option>
               <option value="PHash">pHash</option>
@@ -51,12 +51,12 @@
             </select>
           </div>
           <div class="mb-3">
-            <label for="hashSize" class="form-label">Hash size</label>
-            <input type="text" id="hashSize" class="form-control" v-model="hashSize"/>
+            <label for="hashSize" class="form-label">Hash size ({{ hashSize }})</label>
+            <input type="range" id="hashSize" class="form-range" min="8" max="16" v-model="hashSize"/>
           </div>
           <div class="mb-3">
-            <label for="distance" class="form-label">Max distance</label>
-            <input type="text" id="hashSize" class="form-control" v-model="distance"/>
+            <label for="distance" class="form-label">Max distance ({{ distance }})</label>
+            <input type="range" id="hashSize" class="form-range" v-model="distance"/>
           </div>
         </div>
         <div class="modal-footer">
